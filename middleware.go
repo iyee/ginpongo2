0e7c59ca89c9cbc557a7ea8d7bd55d14cddc64a6
iyee <tuxyik@gmail.com>
2015-05-25 16:41:39 +0800

    Compatible with Gin 1.0 API changes

--- middleware.go
+++ middleware.go
@@ -4,20 +4,20 @@ import (
 	"net/http"
 
 	"github.com/flosch/pongo2"
-	. "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
-func Pongo2() HandlerFunc {
-	return func(c *Context) {
+func Pongo2() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		c.Next()
 
-		templateName, templateNameError := c.Get("template")
+		templateName, exist := c.Get("template")
 		templateNameValue, isString := templateName.(string)
 
-		if templateNameError == nil && isString {
-			templateData, templateDataError := c.Get("data")
+		if exist && isString {
+			templateData, exist := c.Get("data")
 			var template = pongo2.Must(pongo2.FromCache(templateNameValue))
-			err := template.ExecuteWriter(getContext(templateData, templateDataError), c.Writer)
+			err := template.ExecuteWriter(getContext(templateData, exist), c.Writer)
 			if err != nil {
 				http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
 			}
@@ -25,8 +25,8 @@ func Pongo2() HandlerFunc {
 	}
 }
 
-func getContext(templateData interface{}, err error) pongo2.Context {
-	if templateData == nil || err != nil {
+func getContext(templateData interface{}, exist bool) pongo2.Context {
+	if templateData == nil || !exist {
 		return nil
 	}
 	contextData, isMap := templateData.(map[string]interface{})
